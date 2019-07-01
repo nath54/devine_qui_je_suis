900138162e0d23ee31cbe2880b3af6cdc6845480
nath54 <cerisara.nathan@gmail.com>
2019-07-01 13:11:53 +0100

    a

--- devine.js
+++ devine.js
@@ -4,7 +4,7 @@ var imgs_p=["1.png","2.png","3.png","4.png","5.png","6.png","7.png","8.png","9.p
 var imgs=[document.getElementById("img1"),document.getElementById("img2"),document.getElementById("img3"),document.getElementById("img4"),document.getElementById("img5"),document.getElementById("img6"),document.getElementById("img7"),document.getElementById("img8"),document.getElementById("img9"),document.getElementById("img10"),document.getElementById("img11"),document.getElementById("img12"),document.getElementById("img13"),document.getElementById("img14"),document.getElementById("img15"),document.getElementById("img16"),document.getElementById("img17"),document.getElementById("img18"),document.getElementById("img19"),document.getElementById("img20"),document.getElementById("img21"),document.getElementById("img22"),document.getElementById("img23"),document.getElementById("img24"),document.getElementById("img25"),document.getElementById("img26"),document.getElementById("img27"),document.getElementById("img28"),document.getElementById("img29"),document.getElementById("img30"),document.getElementById("img31"),document.getElementById("img32")];
 var isaff=[true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true];
 
-var noms1=["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31","32"];
+var noms1=["Superman","Batman","Wonder Woman","Flash","Green Lantern","Aquaman","Captain Marvel","Green Arrow","Hulk","Thor","Iron Man","Ant Man","Captain America","Spider Man","Wolverine","Le Cyclope","Iceberg","Angel","Le Fauve","La Torche","La chose","Le Surfer D'argent","Deadpool","Le Joker","Lex Luthor","Venom","Dr Octopus","Le bouffon vert","Magneto","Thanos","Loki","Dr Fatalis"];
 var noms2=["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31","32"];
 var noms3=["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31","32"];
 var noms4=["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31","32"];
@@ -24,7 +24,7 @@ function Aff_Image(i)
     }
     else
     {
-        imgs[i].setAttribute("src",dimgs+"dos.png");
+        imgs[i].setAttribute("src",dimgs+"dos1.png");
     }
     
 }
@@ -32,6 +32,7 @@ function Aff_Image(i)
 for(x=0;x<32;x++)
 {
     Aff_Image(x);
+    imgs[x].setAttribute("text",noms_p[x])
 }
 
 function Get_nb_returned()
